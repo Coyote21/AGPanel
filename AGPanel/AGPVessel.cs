2f0dac0fd32013f4db479a4aefacc0052b094c03
Coyote21 <hizuko@me.com>
2021-03-20 11:45:42 +1100

    Various attempts to get Saving/Loading to persistent.sfs to work correctly. Still not working.

--- AGPanel/AGPVessel.cs
+++ AGPanel/AGPVessel.cs
@@ -36,11 +36,21 @@ namespace AGPanel
             if( validVesselTypes.Contains(this.Vessel.vesselType) && this.Vessel.isActiveVessel )
             {
                 Debug.Log("AGPanel.AGPVessel: VSMSave: vesselType:" + this.Vessel.vesselType.ToString() + " and isActive = " + this.vessel.isActiveVessel);
-                node.ClearValues();
+                //node.ClearValues();
+                String tmpString = "blah";
                 for (int i = 1; i < AGPanel.agLabelMap.Count; i++)
                 {
-                    node.AddValue("Action Group " + i, (AGPanel.agLabelMap[i].Length > 0 ? AGPanel.agLabelMap[i] : "badness"));
-                    Debug.Log("AGPanel.AGPVessel: VSMSave: Saved:" + AGPanel.agLabelMap[i] + " to Action Group " + i);
+                    
+                    if (node.TryGetValue("ActionGroup_" + i, ref tmpString)) 
+                    {
+                        node.SetValue("ActionGroup_" + i, (AGPanel.agLabelMap[i].Length > 0 ? AGPanel.agLabelMap[i] : "badness"));
+                        Debug.Log("AGPanel.AGPVessel: VSMSave: Set:" + AGPanel.agLabelMap[i] + " to ActionGroup_" + i);
+                    }
+                    else
+                    {
+                        node.AddValue("ActionGroup_" + i, (AGPanel.agLabelMap[i].Length > 0 ? AGPanel.agLabelMap[i] : "badness"));
+                        Debug.Log("AGPanel.AGPVessel: VSMSave: Added:" + AGPanel.agLabelMap[i] + " to ActionGroup_" + i);
+                    }
                 }
             }
             
@@ -48,27 +58,79 @@ namespace AGPanel
             return node;
         }
 
-        //public override void VSMLoad(ConfigNode node)
-        //{
-        //    if ( validVesselTypes.Contains(this.Vessel.vesselType) && this.vessel.isActiveVessel )
-        //    {
-        //        Debug.Log("AGPanel.AGPVessel: VSMLoad: vesselType:" + this.Vessel.vesselType.ToString() + " and isActive = " + this.vessel.isActiveVessel);
-        //        String tmpString, logString;
-        //        ConfigNode workNode = new ConfigNode();
-        //
-        //        Debug.Log("VMSLoad: node:" + node.ToString());
-        //
-        //        
-        //        //for (int i = 1; i < AGPanel.agLabelMap.Count; i++)
-        //        //{
-        //        //    logString = AGPanel.agLabelMap[i];
-        //        //    Debug.Log("Attempting to get Value Action Group " + i + " from node " + this.vessel.id);
-        //        //    tmpString = node.GetValue("Action Group " + i);
-        //        //    AGPanel.agLabelMap[i] = (tmpString.Length > 0 ? tmpString : AGPanel.agLabelMap[i]);
-        //        //    Debug.Log("dictUpdate: index:" + i + " changed from " + logString + " to " + AGPanel.agLabelMap[i]);
-        //        //}
-        //    }
-        //}
+        public override void VSMLoad(ConfigNode node)
+        {
+            if ( validVesselTypes.Contains(this.Vessel.vesselType) && this.vessel.isActiveVessel )
+            {
+                Debug.Log("AGPanel.AGPVessel: VSMLoad: vesselType:" + this.Vessel.vesselType.ToString() + " and isActive = " + this.vessel.isActiveVessel);
+                //String tmpString, 
+                String logString;
+                String tmpString = "blkah";
+
+                Debug.Log("VSMLoad: node:" + node.ToString());
+        
+                for (int i = 1; i < AGPanel.agLabelMap.Count; i++)
+                {
+                    logString = AGPanel.agLabelMap[i];
+                    Debug.Log("Attempting to get Value ActionGroup_" + i + " from node " + this.vessel.id);
+                    //tmpString = node.GetValue("ActionGroup_" + i);
+                    if (node.TryGetValue("ActionGroup_" + i, ref tmpString)) {
+                        AGPanel.agLabelMap[i] = tmpString;
+                    }
+                    //AGPanel.agLabelMap[i] = (tmpString.Length > 0 ? tmpString : AGPanel.agLabelMap[i]);
+                    Debug.Log("dictUpdate: index:" + i + " changed from " + logString + " to " + AGPanel.agLabelMap[i]);
+                }
+            }
+        }
+
+        public override void VSMDestroy()
+        {
+            base.VSMDestroy();
+            //Remove node from file
+        }
+
+        public override void OnUnloadVessel()
+        {
+            base.OnUnloadVessel();
+            //Reset LabelMap Dictionary?
+
+            // Not seen this called, tested active vessel to space centre.
+
+            if (validVesselTypes.Contains(this.Vessel.vesselType) && this.vessel.isActiveVessel)
+                Debug.Log("AGPanel.AGPVessel: OnUnLoadVessel: this.vessel.id = " + this.vessel.id.ToString());
+        }
+
+        public override void OnLoadVessel()
+        {
+            base.OnLoadVessel();
+            //Put stuff here instead of using VSMLoad?
+            // tested! this method is called correctly when loading vessel
+
+            if (validVesselTypes.Contains(this.Vessel.vesselType) && this.vessel.isActiveVessel)
+                Debug.Log("AGPanel.AGPVessel: OnLoadVessel: this.vessel.id = " + this.vessel.id.ToString());
+
+        }
+
+        protected override void OnSave(ConfigNode node)
+        {
+            base.OnSave(node);
+
+            // Tested! this method is called correctly and regularly
+
+            if (validVesselTypes.Contains(this.Vessel.vesselType) && this.vessel.isActiveVessel)
+                Debug.Log("AGPanel.AGPVessel: OnSave: this.vessel.id = " + this.vessel.id.ToString());
+
+        }
+
+        protected override void OnLoad(ConfigNode node)
+        {
+            base.OnLoad(node);
+
+            // Never seen called (tested load persistent.sfs, new vessel, recover vessel.)
+
+            if (validVesselTypes.Contains(this.Vessel.vesselType) && this.vessel.isActiveVessel)
+                Debug.Log("AGPanel.AGPVessel: OnLoad: this.vessel.id = " + this.vessel.id.ToString());
+        }
 
     }
 }
